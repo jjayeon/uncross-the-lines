64cf172e2a0a5c83b33a88cf56c1556675eac5fd
jjayeon <autumn.chiu.136@gmail.com>
2020-08-14 21:48:56 -0400

    added under construction message

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ import { SVG } from '@svgdotjs/svg.js'
 var draw = SVG().addTo('#drawing').size("100%", "100%");
 
 var bg = draw.rect("100%", "100%").attr({ fill: "rgb(255, 240, 240)" });
+
+var text = draw.text("this website is still under construction. \nthank you for checking in~");
