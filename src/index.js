4e7519a22aaad5d66e1e4d11bc90032016ceaf28
jjayeon <autumn.chiu.136@gmail.com>
2020-08-15 09:33:32 -0400

    reset button; currently flips color on click

--- src/index.js
+++ src/index.js
@@ -1,7 +1,52 @@
 import { SVG } from '@svgdotjs/svg.js'
 
-var draw = SVG().addTo('#drawing').size("100%", "100%");
+function makeDraw() {
+    return SVG().addTo('#drawing').size('100%', '100%');
+}
 
-var bg = draw.rect("100%", "100%").attr({ fill: "rgb(255, 240, 240)" });
+function makeBG(draw) {
+    return draw.rect('100%', '100%')
+	.back()
+	.on('success',  function() {
+	    this.fill({ color: 'rgb(240, 255, 240)' })})
+	.on('failure', function() {
+	    this.fill({ color: 'rgb(255, 240, 240)' })})
+	.fire('failure');
+}
 
-var text = draw.text("this website is still under construction. \nthank you for checking in~");
+function makeResetButton(draw, w, h, bg) {
+    var out = draw.group().size(w, h);
+
+    out.data('success', true, true)
+	.click(function() {
+	    if (this.data('success')) {
+		bg.fire('success');
+		this.data('success', false, true);
+	    }
+	    else {
+		bg.fire('failure');
+		this.data('success', true, true);
+	    }
+	});
+    
+    out.rect(w, h)
+	.front()
+	.fill({ color: 'rgb(255, 112, 77)' });
+
+    out.text('reset').center(w/2, h/2);
+
+    return out;
+}
+
+function go() {
+    const buffer = 40;
+    
+    const buttonWidth = 80;
+    const buttonHeight = 40;
+    
+    const draw = makeDraw();
+    const bg = makeBG(draw);
+    const button = makeResetButton(draw, buttonWidth, buttonHeight, bg);
+}
+
+go();
